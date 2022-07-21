0526a4da38f2b744b129a74cd06f5349536b6cbd
gilcoa <109535907+gilcoa@users.noreply.github.com>
2022-07-21 14:13:46 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,10 +27,10 @@ streamlit.dataframe(fruits_to_show)
 # New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-if not fruit_choice:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
     streamlit.write('The user entered ', fruit_choice)
-else:    
+  else:    
     # import requests
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
     # streamlit.text(fruityvice_response.json()) # just writes the data to the screen
